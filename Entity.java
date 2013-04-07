8c124cac14dcbce7acf4a9fcfd25bffbc92262fc
brandr <magicalfarmrancherseries@yahoo.com>
2013-04-07 16:29:32 -0700

    Create Entity.java

---
+++ Entity.java
@@ -0,0 +1,31 @@
+
+public class Entity {
+
+  public void setPosition(int xPos, int yPos){	//TODO: put in error handling for out-of-bounds areas
+		currentTile = currentLevel.layout[xPos][yPos];
+		currentTile.setIcon(icon);
+	}
+	
+	public void setIcon(char icon){
+		this.icon=icon;
+	}
+	
+	public int getXPos(){
+		return currentTile.xCoord;
+	}
+	
+	public int getYPos(){
+		return currentTile.yCoord;
+	}
+	
+	public Level getCurrentLevel() {
+		return currentLevel;
+	}
+	public void setCurrentLevel(Level currentLevel) {
+		this.currentLevel = currentLevel;
+	}
+
+	private char icon;
+	protected Level currentLevel;
+	protected Tile currentTile;
+}
