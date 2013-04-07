1b90092ab2453ddb64c6a12a4f0e9023684d0303
brandr <magicalfarmrancherseries@yahoo.com>
2013-04-07 16:31:36 -0700

    Update and rename room.java to Room.java

--- Room.java
+++ Room.java
@@ -1,6 +1,6 @@
-public class room { 
+public class Room { 
 
-  public tile [][] tList;
+  public Tile [][] tList;
   public int floor;
   public int coord1x;  //Bottom left corner of room (All rooms are going to be rectangular at this point)
   public int coord1y;
@@ -9,7 +9,7 @@ public class room {
   public int isConnected;
   
 	//Constructor
-  public room(int x, int y, int z, int a, int b, tile [][] layout) {
+  public Room(int x, int y, int z, int a, int b, Tile [][] layout) {
 	floor = x;
 	coord1x = y;
 	coord1y = z;
@@ -19,7 +19,7 @@ public class room {
 	}
 	
 	//Add tiles to the rooms tile list
-  public void addTiles(tile [][] layout) {
+  public void addTiles(Tile [][] layout) {
 
    for (int i = coord1x; i < coord2x; i++) {
 	for (int j = coord1y; j < coord2y; j++) {
@@ -27,4 +27,5 @@ public class room {
 	  layout[i][j].setIcon('_');
 	}
    }
+}
 }
