8c57af02802de4fe79978b0f0131db6adf6efc07
brandr <magicalfarmrancherseries@yahoo.com>
2013-04-08 14:17:26 -0700

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,18 +1,25 @@
 
 public class Player extends Monster{
 
-  public Player() {
+	public static final char PLAYERICON='@';
+	
+	public Player() {
 		name = null;
-		hitPoints[0]=0;
-		hitPoints[1]=0;
-		baseDamage = 0;
+		setIcon(PLAYERICON);
 	}
 	
 	public Player(String name) {
 		this.name = name;
-		hitPoints[0]=0;
-		hitPoints[1]=0;
-		baseDamage = 0;
+		setIcon(PLAYERICON);
+	}
+
+	public String getInfo() {
+		String info="";
+		if(name!=null)
+			info+="Name: " + name+"\n";
+		if(currentHp()>0)
+			info+="Hitpoints: "+showHitPoints();
+		return info;
 	}
 	
 	//move functions either override monster moves or work the same way but are called by keyboard input
