8a7c00f270e5965885196e9aaa8afe4ae9ff2d6a
brandr <magicalfarmrancherseries@yahoo.com>
2013-04-10 17:53:54 -0700

    Update Consumable.java

--- Consumable.java
+++ Consumable.java
@@ -1,34 +1,39 @@
 
-public class Consumable extends Item{
+public class Consumable extends Item{	//currently written assuming potions.
 
-  public Consumable(){
-		consumable=true;
-		
+	
+	
+	public Consumable(){
 		name = null;
+		consumable=true;
 		effect = new Effect();
+		//setIcon(STANDARDCONSUMABLEICON);
 	}
 	
 	public Consumable(String name){
-		consumable=true;
-		
 		this.name=name;
+		consumable=true;
 		effect = new Effect();
+		//setIcon(STANDARDCONSUMABLEICON);
 	}
 	
 	public Consumable(String name, int effectIndex, int value){
-		consumable=true;
-		
 		this.name=name;
+		consumable=true;
 		effect=new Effect(effectIndex);
 		effect.value=value;
+		//setIcon(STANDARDCONSUMABLEICON);
+	}
+	
+	public String toString(){
+		return name;
 	}
 	
 	@Override
 	public void use(Monster target) {
 		effect.takeEffect(target);
-		
 	}
 
-	protected Effect effect;
+	protected Effect effect;	//both potions and food have effects.
 	//public static boolean consumable=true;
 }
